93556ada90d31629d52bfcf5dd62dc6f9a240b64
Philip Han <phan@indeed.com>
2024-02-13 11:45:08 -0600

    Seeds on server start

--- src/server.js
+++ src/server.js
@@ -8,7 +8,15 @@ const listener = () => console.log(`Listening on Port ${PORT}!`);
 knex.migrate
   .latest()
   .then((migrations) => {
-    console.log("migrations", migrations);
-    app.listen(PORT, listener);
+      console.log("migrations", migrations);
+  })
+  .then(() => {
+      return knex.seed.run();
+  })
+  .then((seeds) => {
+      console.log('seeds', seeds);
+  })
+  .then(() => {
+      app.listen(PORT, listener);
   })
   .catch(console.error);
